383992906ceeab07d057b5d88622e4f496c02706
OjhaTechinal <73268531+Dhananjay-built@users.noreply.github.com>
2021-07-13 15:14:00 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    // Enter Your Methods Here
+   public static void main(String[] args) {
+       // Enter Your Code Here
+       System.out.print("Hello World");
+       System.out.print("jai Ganesh");
+   }
+
+}
